21c83181439430b3b39e9da20da714bc095e270f
kbea-sol <0beack69@solent.ac.uk>
2025-09-24 14:49:57 +0100

    made cats do labour

--- src/main.kt
+++ src/main.kt
@@ -5,4 +5,18 @@ fun main() {
     clyde.walk()
     println(binnie)
     println(clyde)
+
+    val flathead = Cat("Flathead", 8 , 7)
+    val other = Cat("Other", 16, 23)
+
+    flathead.eat()
+    flathead.eat()
+    println(flathead)
+
+    other.walk()
+    other.walk()
+    other.walk()
+    other.walk()
+    println(other)
+
 }
