a05f41374e3ee658c12ea3ea3e48922db24b31ce
kbea-sol <0beack69@solent.ac.uk>
2025-10-01 14:27:12 +0100

    added paramaterised eat and walk funcs

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,7 +1,21 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
 
-    fun walk() {
-        weight--
+    fun walk(dist : Int) : Boolean{
+        if((weight - dist) >= 5)
+        {
+            weight -= dist
+            return true
+        }
+        return false
+    }
+
+    fun eat(food : Int) : Boolean{
+        if((weight + food) <= 20)
+        {
+            weight += food
+            return true
+        }
+        return false
     }
 
     override fun toString() : String {
--- src/main.kt
+++ src/main.kt
@@ -2,37 +2,29 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
-    binnie.walk()
-    clyde.walk()
     println(binnie)
     println(clyde)
 
     val flathead = Cat("Flathead", 7 , 10)
-    val cupra = Cat("Cupra", 2, 7)
-    val steven = Cat("Steven", 4, 15)
+    val cupra = Cat("Cupra", 2, 12)
 
-    flathead.eat()
-    flathead.eat()
-    println(flathead)
+    if(!cupra.eat(20))
+    {
+        println("Invalid food amount entered")
+    }
+    if(!cupra.walk(20))
+    {
+        println("Invalid distance entered")
+    }
+    if(!cupra.walk(5))
+    {
+        println("Invalid distance entered")
+    }
+    if(!cupra.eat(5))
+    {
+        println("Invalid distance entered")
+    }
 
-    cupra.walk()
-    cupra.walk()
-    cupra.walk()
-    cupra.walk()
     println(cupra)
 
-    val cat1 = catReproductionProgram(flathead, cupra)
-    val cat2 = catReproductionProgram(cupra, clyde)
-    val cat3 = catReproductionProgram(steven, cat1)
-    val cat4 = catReproductionProgram(cat3, cat2)
-    println(cat1)
-    println(cat2)
-    println(cat3)
-    println(cat4)
-}
-
-fun catReproductionProgram(subject1 : Cat, subject2 : Cat) : Cat{
-    var name = subject1.name.slice(0..(subject1.name.length/2).toInt())
-    name += subject2.name.slice((subject2.name.length/2)..<subject2.name.length)
-    return Cat(name, 0, 5 + kotlin.random.Random.nextInt(-3,3))
 }
